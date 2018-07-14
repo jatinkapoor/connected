993dc6e7f09c7dc22ed6a378de46283d006f5cbd
Holger Mueller <9jholger@gmail.com>
2018-07-14 13:52:04 -0500

    fixed some errors

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import Home from './containers/Home/Home';
 import Auth from './containers/Auth/Auth';
 import Messages from './pages/Messages/Messages';
-
+import CheckInFeed from './pages/CheckInFeed/CheckInFeed';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Auth />
+        <CheckInFeed />
       </div>
     );
   }
--- client/src/pages/CheckInFeed/CheckInFeed.js
+++ client/src/pages/CheckInFeed/CheckInFeed.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Grid, GridCell, GridInner } from 'rmwc/Grid';
+import React, {Component} from "react";
+import {GridCell, GridInner } from 'rmwc/Grid';
 import NavBar from '../../components/UI/NavBar/NavBar';
 import "./CheckInFeed.css";
 
 class CheckInFeed extends Component {
 	state = {
-		usersCheckedIn=[]
+		usersCheckedIn: []
 	};
 
 	componentDidMount = () => {
@@ -18,27 +18,37 @@ class CheckInFeed extends Component {
 
 	render() {
 		return (
-			<div>
+			<React.Fragment>
 				<NavBar />
-				<Grid fluid>
-					{this.state.usersCheckedIn.map(checkedInUser => (
+				<GridCell className="grid">
 						<GridInner span="12">
-							<GridCell span="4">
+							<GridCell span="4">Hello
+							</GridCell>
+							<GridCell span="4">World
+							</GridCell>
+							<GridCell span="4">Message
+							</GridCell>
+						</GridInner>
+{	/*				{this.state.usersCheckedIn.map(checkedInUser => (
+						<GridCell span="12">
+						<GridInner>
+							<GridCell span="4">Hello
 							{checkedInUser.firstName}
 							</GridCell>
-							<GridCell span="4">
+							<GridCell span="4">World
 							{checkedInUser.lastName}
 							</GridCell>
-							<GridCell span="4">
+							<GridCell span="4">Message
 							{checkedInUser.date}
 							</GridCell>
 						</GridInner>
-			))}
-				</Grid>
-			</div>
-		)
+						</GridCell>
+			))} */}
+				</GridCell>
+			</React.Fragment>
+		);
+		
 	}
-	
 };
 
 export default CheckInFeed;
