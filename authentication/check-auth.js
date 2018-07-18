924446ee39e96c0c8682a86be742f8ac1b8601a0
Jatin.Kapoor <jatin.kapoor@target.com>
2018-07-17 20:47:42 -0500

    adding code for posts and comments

--- authentication/check-auth.js
+++ authentication/check-auth.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    req.userDate = decoded;
+    req.userData = decoded;
+    console.log('in user auth');
     next();
   } catch(error) {
     return res.status(401).json({
---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', (req, res) => {
+
+  
+
+
+});
+
+module.exports = router;
