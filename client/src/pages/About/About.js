0fa07ae1bf0c7b9ef6e304322796179cf27a198a
wdarsow <wade.darsow@gmail.com>
2018-07-17 21:16:40 -0500

    start of about me page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, GridCell } from 'rmwc/Grid';
 import NavBar from '../../components/UI/NavBar/NavBar';
-
+import './about.css';
 class About extends Component {
     render() {
         return(
             <div>
                 <NavBar />
-                <h1>About</h1>
+                <Grid >
+                    <GridCell phone="0" align="middle" tablet="1" desktop="4"> 
+                        About
+                    </GridCell>
+                </Grid>
+                    <h1 className="about">About</h1>
             </div>
+            // look into typography
         )
     }
 }
